f23f6ee0491e39c06f117f3d2275a2a19f820301
Aviad Klein <aviad.klein@plarium.com>
2024-06-05 13:52:01 +0300

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fluq",  
-    version="0.1.0",
+    version="0.1.1",
     author="Aviad Klein",
     author_email="aviad.klein@gmail.com",
     description="Python style api for heavy SQL writers",
