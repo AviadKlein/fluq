56fa4500000a1fcd3d5864684121037565bb0a73
Aviad Klein <aviad.klein@plarium.com>
2024-06-06 10:53:18 +0300

    fixes to setup.py

--- fluq/__init__.py
+++ fluq/__init__.py
@@ -5,10 +5,4 @@ from .sql import *
 
 __all__ = ['sql', 'column']
 
-def __read_version():
-    import os
-    with open('./__version__.txt') as f:
-        return f.read().strip()
-
-__version__ = __read_version()
-del(__read_version)
+__version__ = "0.1.2"
