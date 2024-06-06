72f479cc3bb093b45d7eff1779f8c36fc3589d41
Aviad Klein <aviad.klein@plarium.com>
2024-06-06 09:21:04 +0300

    added version comparison between setup.py and pyproject.toml

--- fluq/__init__.py
+++ fluq/__init__.py
@@ -4,3 +4,11 @@ from .column import *
 from .sql import *
 
 __all__ = ['sql', 'column']
+
+def __read_version():
+    import os
+    with open('./__version__.txt') as f:
+        return f.read().strip()
+
+__version__ = __read_version()
+del(__read_version)
--- setup.py
+++ setup.py
@@ -1,8 +1,38 @@
 from setuptools import setup, find_packages
 
+def __read_version():
+    with open('__version__.txt') as f:
+        return f.read().strip()
+    
+def __validate_version_is_aligned_with_toml():
+    import re
+    toml_version = None
+    with open('pyproject.toml') as f:
+        for line in f.readlines():
+            m = re.match('''^version = "([0-9\.]+)"''', line)
+            match m:
+                case None:
+                    pass
+                case _:
+                    if len(m.groups()) == 0:
+                        raise Exception("matched 0 versions... check the pyproject.toml file")
+                    elif len(m.groups()) == 1:
+                        toml_version = m.groups()[0]
+                    else:
+                        raise Exception("matched multiple versions... check the pyproject.toml file")
+    if toml_version is None:
+        raise Exception("found no 'version' entry in the pyproject.toml file")
+    pkg_version = __read_version()
+    assert toml_version == pkg_version, f"pyproject.toml version does not match __version__.txt, got {toml_version=} and {pkg_version=}"
+
+
+__validate_version_is_aligned_with_toml()
+__version__ = __read_version()
+del(__read_version)
+
 setup(
     name="fluq",  
-    version="0.1.1",
+    version=__version__,
     author="Aviad Klein",
     author_email="aviad.klein@gmail.com",
     description="Python style api for heavy SQL writers",
