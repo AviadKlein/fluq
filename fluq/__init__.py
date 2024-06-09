650aa669f57bdeb18eb428a20c71112e953b83f7
Aviad Klein <aviad.klein@plarium.com>
2024-06-09 15:19:36 +0300

    version bump to v0.1.4

--- fluq/__init__.py
+++ fluq/__init__.py
@@ -1,8 +1,7 @@
 # fluq/__init__.py
 
-from .column import *
 from .sql import *
 
-__all__ = ['sql', 'column']
+__all__ = ['sql']
 
 __version__ = "0.1.3"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fluq",  
-    version="0.1.3",
+    version="0.1.4",
     author="Aviad Klein",
     author_email="aviad.klein@gmail.com",
     description="Python style api for heavy SQL writers",
